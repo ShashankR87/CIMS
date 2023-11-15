cce349fe669804ba6ae48b941ada7f316058d86f
mtaulet <tauletsa@usc.edu>
2023-11-15 15:24:56 -0800

    code improvements

--- controller.py
+++ controller.py
@@ -6,54 +6,34 @@ import sys
 import time
 import numpy as np
 import numpy as np
-import keras.backend.tensorflow_backend as backend
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten
-from keras.optimizers import Adam
-from keras.callbacks import TensorBoard
 import tensorflow as tf
 from collections import deque
 import time
 import random
 from tqdm import tqdm
 import os
-from PIL import Image
-import cv2
 
-# TODO: Create requirements.txt
+from dql import DQL
+
+# Environment settings
+EPISODES = 20_000
+
+# Exploration settings
+epsilon = 1  # not a constant, going to be decayed
+EPSILON_DECAY = 0.99975
+MIN_EPSILON = 0.001
 
 COLLISION_PENALTY = 10_000
 DESTINATION_REWARD = 1000
-
 N_CARS = 2
 VEHICLE_MODEL = 'vehicle.jeep.wrangler_rubicon'
 A_MIN = 0
 A_MAX = 10
-ACTION_SPACE_SIZE = 10
 T = 20
 
-ACC_MAP = {
-    0: -10,
-    1: -9,
-    2: -8,
-    3: -7,
-    4: -6,
-    5: -5,
-    6: -4,
-    7: -3,
-    8: -2,
-    9: -1,
-    10: 0,
-    11: 1,
-    12: 2,
-    13: 3,
-    14: 4,
-    15: 5,
-    16: 6,
-    17: 7,
-    18: 8,
-    19: 9
-}
+ACC_MAP = {i: i - 10 for i in range(20)}   # {0: -10, 1: -9, ... 18: 8, 19: 9}
+ACTION_SPACE_SIZE = len(ACC_MAP)           # 20
+
 '''
 Paths to make car move through the intersection (Direction is relative to the fixed spectator pov)
 N->E (49, 27)
@@ -199,10 +179,18 @@ class CarlaEnv:
 
 
     def reset(self):
+        '''Returns array [v_1, p_1, v_2, p_2, ...v_n, p_n]'''
+
         self.destroy_all_cars()
         self.spawn_n_cars()
         self.episode_step = 0
-        return  
+        current_state = []
+
+        for car in self.vehicles:
+            current_state.append(car["vel"])
+            current_state.append(car["path"])
+
+        return current_state
 
 
     def step(self, acc_list):
@@ -229,6 +217,63 @@ class CarlaEnv:
         return reward, done
 
 
+
+    def run_episodes(self, agent):
+            
+        for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
+
+            agent.tensorboard.step = episode
+            episode_reward = 0
+            step = 1
+
+            # Reset environment and get initial state
+            current_state = self.reset()
+
+            done = False
+            while not done:
+                if np.random.random() > epsilon:
+                    # Get action from Q table - each action value should be an integer from 0 to 19
+                    action1 = np.argmax(agent.get_qs(current_state)[:ACTION_SPACE_SIZE])
+                    action2 = np.argmax(agent.get_qs(current_state)[ACTION_SPACE_SIZE:])
+                    action = [action1, action2]
+                else:
+                    # Get random action
+                    action = [np.random.randint(0, self.ACTION_SPACE_SIZE),
+                              np.random.randint(0, self.ACTION_SPACE_SIZE)]
+
+                new_state, reward, done = self.step(action)
+
+                # Transform new continous state to new discrete state and count reward
+                episode_reward += reward
+
+                # if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
+                #     env.render()
+
+                # Every step we update replay memory and train main network
+                agent.update_replay_memory((current_state, action, reward, new_state, done))
+                agent.train(done, step)
+
+                current_state = new_state
+                step += 1
+
+            # Append episode reward to a list and log stats (every given number of episodes)
+            ep_rewards.append(episode_reward)
+            if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+                average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
+                min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+                max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+                agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
+
+                # Save model, but only when min reward is greater or equal a set value
+                if min_reward >= MIN_REWARD:
+                    agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+
+            # Decay epsilon
+            if epsilon > MIN_EPSILON:
+                epsilon *= EPSILON_DECAY
+                epsilon = max(MIN_EPSILON, epsilon)
+
+
 def main():
 
     # carla setup
@@ -237,9 +282,16 @@ def main():
     # fix spectator on top of intersection
     carla_env.spectator_setup(x=-47, y=16.8, z=60)
 
-    agent = DQN()
+    agent = DQL()
 
-    run_episodes(agent)
+    random.seed(1)
+    np.random.seed(1)
+    tf.set_random_seed(1)
+
+    if not os.path.isdir('models'):
+        os.makedirs('models')
+
+    carla_env.run_episodes(agent)
 
     # Control car by varying throttle manually
     carla_env.control_manual(vehicle, spawn_point_collision=79)
